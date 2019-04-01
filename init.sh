dbbac13121f6cdeefd8b6852fc7a545000033718
Linette Tay Wei Ping <linette715@gmail.com>
2019-04-01 11:08:29 +0800

    First commit

---
+++ init.sh
@@ -0,0 +1,11 @@
+
+## Install xcode (for make and gcc)
+xcode-select --install
+
+## Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Install Mac AppStore CLI
+brew update
+brew install mas
+mas signin email@email.com
